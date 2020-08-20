c2359ba12b6ec2d7bf5448b71257a9f782bd90fc
Mogan <moganvig90@gmail.com>
2020-08-20 22:37:12 +0530

    indication with app mapping

--- eMTE.Temperatuer.BusinessLayer/DTO/HealthMeasure/Request/CreateHealthMeasure.cs
+++ eMTE.Temperatuer.BusinessLayer/DTO/HealthMeasure/Request/CreateHealthMeasure.cs
@@ -7,8 +7,8 @@ namespace eMTE.Temperature.BusinessLayer.DTO.HealthMeasure.Request
         public string TemperatureUnit { get; set; }
         public int SlotNumber { get; set; }
         public bool Cough { get; set; }
-        public bool Sneezing { get; set; }
-        public bool RunnyNose { get; set; }
+        public bool Sneezing { get; set; } // app mapping: soreThroat
+        public bool RunnyNose { get; set; } // app mapping: cold
         public bool ShortnessBreath { get; set; }
         public string OxygenSaturation { get; set; }
         public string HeatRate { get; set; }
